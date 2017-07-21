14ffb7952b0e4c98d3cde7a42e96525facb4a4df
jais210 <jakeline_210@hotmail.com>
2017-07-21 17:59:33 -0500

    2funciones

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ for (var i = 0; i < asientos.length; i++) {
   asientos[i].addEventListener("click", redirect, false);
 }
 function redirect(event) {
-
   asiento = event.target.textContent;
 
   var mostrar = document.getElementById("mostrar");
@@ -15,37 +14,42 @@ function redirect(event) {
       ? "transparent"
       : "#F8ED50";
 }
-function buscar(ingreso) {
-  var ingreso = document.getElementById("nro_dni").value;
-  var operacion = ingreso * 2;
-  document.getElementById("nro_dni").value = operacion;
+function buscar() {
+  var ingreso = document.getElementById("dnibuscar").value;
+  for (var i = 0; i < 32; i++) {
+    if (pasajero[i] != undefined && pasajero[i].dni == ingreso) {
+      document.getElementById("nombre").value = pasajero[i].nombre;
+      document.getElementById("apellido").value = pasajero[i].apellido;
+      document.getElementById("dni").value = pasajero[i].dni;
+    }
+  }
 }
-var pasajero = new Array(32);
-for(var i=0; i<32; i++){
-  pasajero[i] = undefined;
+function mostrarLista(pasajero) {
+  var resultado = "";
 }
 
+var pasajero = new Array(32);
+for (var i = 0; i < 32; i++) {
+  pasajero[i] = undefined;
+}
 
 function reservar() {
   var nombre = document.getElementById("nombre").value;
   var apellido = document.getElementById("apellido").value;
   var dni = document.getElementById("dni").value;
-       
-  pasajero [asiento-1]= {
-    nombre:nombre,
-    apellido: apellido,
-    dni:dni
 
+  pasajero[asiento - 1] = {
+    nombre: nombre,
+    apellido: apellido,
+    dni: dni
   };
-   alert("registrado")
-   limpiar();
+  alert("registrado");
+  limpiar();
   console.log(pasajero);
 }
 
-function limpiar(){
-   document.getElementById("nombre").value="";
-   document.getElementById("apellido").value= "";
-   document.getElementById("dni").value = "";
-
-
+function limpiar() {
+  document.getElementById("nombre").value = "";
+  document.getElementById("apellido").value = "";
+  document.getElementById("dni").value = "";
 }
