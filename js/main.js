44a1c4399364a0fca319c2ad40ecd89f2f640e13
jais210 <jakeline_210@hotmail.com>
2017-07-24 09:36:12 -0500

    modificacion de nombres de carpeta

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-var asiento = -1;
+var _asiento = -1;
 var asientos = document.getElementsByTagName("td");
 
 for (var i = 0; i < asientos.length; i++) {
   asientos[i].addEventListener("click", redirect, false);
 }
 function redirect(event) {
-  asiento = event.target.textContent;
+  _asiento = event.target.textContent;
 
   var mostrar = document.getElementById("mostrar");
-  mostrar.innerHTML = asiento;
+  mostrar.innerHTML = _asiento;
   event.target.style.backgroundColor =
     event.target.style.backgroundColor == "rgb(248, 237, 80)"
       ? "transparent"
@@ -38,7 +38,7 @@ function reservar() {
   var apellido = document.getElementById("apellido").value;
   var dni = document.getElementById("dni").value;
 
-  pasajero[asiento - 1] = {
+  pasajero[_asiento - 1] = {
     nombre: nombre,
     apellido: apellido,
     dni: dni
