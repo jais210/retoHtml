4e6955990959cbc5a772226b2c1ee43e2319f6eb
jais210 <jakeline_210@hotmail.com>
2017-07-25 09:29:46 -0500

    modificacion de variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,33 +4,32 @@ var asientos = document.getElementsByTagName("td");
 for (var i = 0; i < asientos.length; i++) {
   asientos[i].addEventListener("click", redirect, false);
 }
+
 function redirect(event) {
   numeroAsiento = event.target.textContent;
 
   var mostrar = document.getElementById("mostrar");
   mostrar.innerHTML = numeroAsiento;
   event.target.style.backgroundColor =
-    event.target.style.backgroundColor == "rgb(248, 237, 80)"
-      ? "transparent"
-      : "#F8ED50";
+    event.target.style.backgroundColor == "rgb(248, 237, 80)" ?
+    "transparent" :
+    "#F8ED50";
 }
+
 function buscar() {
   var ingreso = document.getElementById("dnibuscar").value;
   for (var i = 0; i < 32; i++) {
-    if (pasajero[i] != undefined && pasajero[i].dni == ingreso) {
-      document.getElementById("nombre").value = pasajero[i].nombre;
-      document.getElementById("apellido").value = pasajero[i].apellido;
-      document.getElementById("dni").value = pasajero[i].dni;
+    if (pasajeros[i] != undefined && pasajeros[i].dni == ingreso) {
+      document.getElementById("nombre").value = pasajeros[i].nombre;
+      document.getElementById("apellido").value = pasajeros[i].apellido;
+      document.getElementById("dni").value = pasajeros[i].dni;
     }
   }
 }
-function mostrarLista(pasajero) {
-  var resultado = "";
-}
 
-var pasajero = new Array(32);
+var pasajeros = new Array(32);
 for (var i = 0; i < 32; i++) {
-  pasajero[i] = undefined;
+  pasajeros[i] = undefined;
 }
 
 function reservar() {
@@ -38,14 +37,14 @@ function reservar() {
   var apellido = document.getElementById("apellido").value;
   var dni = document.getElementById("dni").value;
 
-  pasajero[numeroAsiento - 1] = {
+  pasajeros[numeroAsiento - 1] = {
     nombre: nombre,
     apellido: apellido,
     dni: dni
   };
   alert("registrado");
   limpiar();
-  console.log(pasajero);
+  console.log(pasajeros);
 }
 
 function limpiar() {
@@ -54,3 +53,21 @@ function limpiar() {
   document.getElementById("dni").value = "";
 }
 
+function mostrarLista() {
+  var lista = "";
+
+  for (var i = 0; i < 32; i++) {
+
+    if (pasajeros[i] != undefined) {
+      document.getElementById("nombre").value = pasajeros[i].nombre;
+      document.getElementById("apellido").value = pasajeros[i].apellido;
+      document.getElementById("dni").value = pasajeros[i].dni;
+    }
+
+    
+
+  }
+
+  return lista;
+
+}
